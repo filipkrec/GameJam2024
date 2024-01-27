be5624018f0401cfff4b2922e5286c502eb6aef2
Ana Sundji <asundji@gmail.com>
2024-01-27 12:28:29 +0100

    CameraController implemented. Basic duck behaviour added.

---
+++ Assets/ANA/Scripts/CameraController.cs
@@ -0,0 +1,47 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Header("Follow Values")]
+    [SerializeField] private Vector3 _followOffset = Vector3.zero;
+    [SerializeField] private Vector3 _followVelocity = Vector3.zero;
+    [SerializeField] private float _followSmoothTime = 0.0f;
+
+    [Header("Defeat Position Values")]
+    [SerializeField] private Vector3 _defeatPositionOffset = Vector3.zero;
+    [SerializeField] private float _defeatAnimationDuration = 0.0f;
+
+    private Transform _cameraTransform = null;
+    private Transform _duckTransform = null;
+
+    private bool _isDuckAlive = true;
+
+    private void Start()
+    {
+        _duckTransform = Duck.Instance.transform;
+        Duck.Instance.SetOnDeathAction(() => _isDuckAlive = false);
+        Duck.Instance.SetOnDeathAction(SwitchToDefeatPosition);
+
+        _cameraTransform = Camera.main.transform;
+        _cameraTransform.position = _duckTransform.position + _followOffset;
+        _cameraTransform.LookAt(_duckTransform.position);
+    }
+
+    private void Update()
+    {
+        if (_isDuckAlive)
+        {
+            Vector3 finalPosition = _duckTransform.position + _followOffset;
+            _cameraTransform.position = Vector3.SmoothDamp(_cameraTransform.position, finalPosition, ref _followVelocity, _followSmoothTime);
+        }
+    }
+
+    private void SwitchToDefeatPosition()
+    {
+        Vector3 duckPosition = _duckTransform.position;
+        _cameraTransform
+            .DOMove(duckPosition + _defeatPositionOffset, _defeatAnimationDuration)
+            .OnUpdate(() => _cameraTransform.LookAt(duckPosition));
+    }
+}
