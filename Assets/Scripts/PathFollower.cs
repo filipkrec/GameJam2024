70756c61ef32e8b8c3ba4b43e8407cc94a60d3d4
Sundji <asundji@gmail.com>
2024-01-28 03:29:38 +0100

    Title screen updated.

--- Assets/Scripts/PathFollower.cs
+++ Assets/Scripts/PathFollower.cs
@@ -5,6 +5,7 @@ public class PathFollower : MonoBehaviour
 {
     [SerializeField] private Transform[] _waypoints = null;
     [SerializeField] private float _pathDuration = 0.0f;
+    [SerializeField] private bool _shouldLookAtTargetPosition = true;
 
     private Vector3[] _positions = null;
 
@@ -13,10 +14,20 @@ public class PathFollower : MonoBehaviour
         _positions = new Vector3[_waypoints.Length];
         for (int index = 0; index < _waypoints.Length; index++) _positions[index] = _waypoints[index].position;
 
-        transform
-            .DOPath(_positions, _pathDuration)
-            .OnWaypointChange((index) => transform.LookAt(_positions[index + 1 > _positions.Length - 1 ? 0 : index + 1]))
-            .SetEase(Ease.Linear)
-            .SetLoops(-1);
+        if (_shouldLookAtTargetPosition)
+        {
+            transform
+                .DOPath(_positions, _pathDuration)
+                .OnWaypointChange((index) => transform.LookAt(_positions[index + 1 > _positions.Length - 1 ? 0 : index + 1]))
+                .SetEase(Ease.Linear)
+                .SetLoops(-1);
+        }
+        else
+        {
+            transform
+                .DOPath(_positions, _pathDuration)
+                .SetEase(Ease.Linear)
+                .SetLoops(-1);
+        }
     }
 }
